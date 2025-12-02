717686d360da98589c0d71f79b54ca7a983623a7
Ana <anab.costac@gmail.com>
2025-12-02 20:48:42 -0300

    update

--- API-Pet-Shop/prisma.config.ts
+++ API-Pet-Shop/prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   // A URL de conexão da database principal vai diretamente aqui.
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
     // Se você usa shadow database para migrações:
     // shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL, 
   },
